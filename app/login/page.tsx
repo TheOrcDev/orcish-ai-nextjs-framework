a6dcb0486f84a166c0a4a8cdc558bb6725537be3
papaurke <urosmiric88@gmail.com>
2024-01-06 12:24:13 +0100

    feat(pages): login page
    Creation of login page

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/components/auth/FormLogin";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">logo</div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
