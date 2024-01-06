9c50f036dbe37fc91e2150011d3fd40100f00340
papaurke <urosmiric88@gmail.com>
2024-01-06 12:21:02 +0100

    feat(ui): login form
    LoginForm component creation

---
+++ lib/actions.ts
@@ -0,0 +1,21 @@
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
