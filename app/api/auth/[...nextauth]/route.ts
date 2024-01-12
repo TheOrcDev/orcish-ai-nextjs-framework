79e74e21dd07c3a313b09bba592e7b7b31e0d4bd
papaurke <urosmiric88@gmail.com>
2024-01-12 22:10:38 +0100

    feat(lib): authorization by Google
    Login enabled

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,3 @@
-"use client";
-
-import { auth } from "@/lib/auth";
-
 export default async function Page() {
-  const session = await auth();
-  return <p>Welcome {session?.user?.name}! </p>;
+  return <p>Welcome! </p>;
 }
