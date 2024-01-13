18a715054a49682a2c82f842aa1d258f3587c3ae
papaurke <urosmiric88@gmail.com>
2024-01-13 22:18:22 +0100

    feat(config): orcish OpenAI Connector
    Installation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 };
 const handler = NextAuth(authOptions);
