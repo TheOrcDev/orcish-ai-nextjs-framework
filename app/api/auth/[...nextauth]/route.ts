b3fd2405d6098398595b08b53387100e8e77ea99
papaurke <urosmiric88@gmail.com>
2024-01-25 00:16:33 +0100

    refactor(config): unecessary code
    Removed console.log

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log(process.env.GOOGLE_CLIENT_ID);
+
 export const authOptions = {
   providers: [
     GoogleProvider({
