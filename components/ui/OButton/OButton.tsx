23b571b5366a09ac1ee46105464f85651cf47e30
papaurke <urosmiric88@gmail.com>
2024-01-25 16:57:44 +0100

    fix(ui): button active state
    Active state fixed

--- components/ui/OButton/OButton.tsx
+++ components/ui/OButton/OButton.tsx
@@ -8,8 +8,8 @@ export default function OButton({ children, active, onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`p-3 bg-white dark:text-black rounded-xl w-40 hover:bg-orange-100 ${
-        active && "bg-orange-300"
+      className={`p-3 dark:text-black rounded-xl w-40 hover:bg-orange-100 ${
+        active ? "bg-orange-300" : "bg-white"
       }`}
     >
       {children}
