4e9260e762bfdcf17feac9d6ceb98b57cfc2005f
papaurke <urosmiric88@gmail.com>
2024-01-31 22:04:03 +0100

    refactor(config): unnecessary code
    Removed webpack config, and .yarn folder

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
