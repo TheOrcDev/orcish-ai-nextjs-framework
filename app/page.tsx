c2daf0078b32c30d9c2ac76ef5137a7eada1bdaa
papaurke <urosmiric88@gmail.com>
2024-03-08 23:29:07 +0100

    fix(ui): empty scroll
    Fixed min h screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { AISelector } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div className="flex flex-col text-center gap-5">
         <h1>Orcish AI Next.js Framework</h1>
         <AISelector />
