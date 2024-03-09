5bb2809b154aa2913b9c97faf83f24b67f792951
papaurke <urosmiric88@gmail.com>
2024-03-09 21:43:55 +0100

    fix(pages): suppressHydrationWarning
    Theme provider console log error

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
