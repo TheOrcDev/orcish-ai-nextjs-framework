b56015de69c86f6fc4764aac7d2c1c4be694399a
papaurke <urosmiric88@gmail.com>
2024-03-09 21:45:00 +0100

    refactor(ui): loading smaller
    More neat loading component

--- components/shared/ui/Loading/Loading.tsx
+++ components/shared/ui/Loading/Loading.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Loading() {
-  return <Image src={"/loading.gif"} alt="Loading" width={200} height={200} />;
+  return <Image src={"/loading.gif"} alt="Loading" width={80} height={80} />;
 }
