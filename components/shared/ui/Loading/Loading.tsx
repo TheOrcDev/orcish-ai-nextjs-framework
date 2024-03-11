ce3c5d8773fd5ebdbc0f8394be7dd4f1e08ad46d
papaurke <urosmiric88@gmail.com>
2024-03-11 18:50:44 +0100

    fix(ui): loading gif
    Recommended message by Next.js for unimportant fixed

--- components/shared/ui/Loading/Loading.tsx
+++ components/shared/ui/Loading/Loading.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Loading() {
-  return <Image src={"/loading.gif"} alt="Loading" width={80} height={80} />;
+  return (
+    <Image
+      unoptimized
+      src={"/loading.gif"}
+      alt="Loading"
+      width={80}
+      height={80}
+    />
+  );
 }
