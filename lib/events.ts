9300e1f32c2044ee1a832eb6bb371aa72c1a1e63
papaurke <urosmiric88@gmail.com>
2024-05-02 21:04:05 +0200

    feat(lib): ctrl + enter for submit
    Added ctrl or cmd

--- lib/events.ts
+++ lib/events.ts
@@ -2,7 +2,7 @@ export const enter = (
   event: React.KeyboardEvent,
   action: () => Promise<void>
 ) => {
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && (event.ctrlKey || event.metaKey)) {
     action();
   }
 };
