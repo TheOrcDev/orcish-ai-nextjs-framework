0f30b9767cea19929f07c0f2e33622022999d407
papaurke <urosmiric88@gmail.com>
2024-05-02 21:41:35 +0200

    refactor(pages): unnecessary div
    removal

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import { AISelector } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="flex w-full flex-col items-center gap-5 text-center">
-        <h1>Orcish AI Next.js Framework</h1>
-        <AISelector />
-      </div>
+    <main className="flex flex-col items-center justify-between gap-5 p-24">
+      <h1>Orcish AI Next.js Framework</h1>
+      <AISelector />
     </main>
   );
 }
