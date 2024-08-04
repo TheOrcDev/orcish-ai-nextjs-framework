ab6eec221d05921760cadbb89051ce753b31e970
papaurke <urosmiric88@gmail.com>
2024-08-04 13:41:35 +0200

    refactor(lib): add try catch to get all tokens function
    safety

--- lib/queries.ts
+++ lib/queries.ts
@@ -3,18 +3,23 @@ import { purchases, tokenSpends } from "@/db/schema";
 import { count, eq, sum } from "drizzle-orm";
 
 export const getTotalTokens = async (email: string): Promise<number> => {
-    const [tokens] = await db.
-        select({ value: sum(purchases.amount) }).
-        from(purchases).
-        where(eq(purchases.email, email));
+    try {
+        const [tokens] = await db.
+            select({ value: sum(purchases.amount) }).
+            from(purchases).
+            where(eq(purchases.email, email));
 
-    if (!tokens.value) {
-        return 0;
+        if (!tokens.value) {
+            return 0;
+        }
+
+        const [tokensSpend] = await db.select({ count: count() }).
+            from(tokenSpends).
+            where(eq(tokenSpends.email, email));
+
+        return +tokens.value - tokensSpend.count;
+    } catch (e) {
+        throw (e);
     }
 
-    const [tokensSpend] = await db.select({ count: count() }).
-        from(tokenSpends).
-        where(eq(tokenSpends.email, email));
-
-    return +tokens.value - tokensSpend.count;
 }
