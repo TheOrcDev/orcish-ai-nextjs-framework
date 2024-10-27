2b9ac97957326b1386d5bce8ae17c0222fdd89f6
OrcDev <urosmiric88@gmail.com>
2024-10-27 19:43:09 +0100

    chore: upgrade to Next.js 15
    Up to latest

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
   "/ai-selector(.*)",
   "/buy-tokens(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
 });
 
 export const config = {
