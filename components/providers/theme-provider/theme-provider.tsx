cc939b60ac933d6e51fbe786c9f16da6be87ba33
OrcDev <urosmiric88@gmail.com>
2025-01-26 22:56:46 +0100

    fix(theme-provider) add new shadcn theme provider

--- components/providers/theme-provider/theme-provider.tsx
+++ components/providers/theme-provider/theme-provider.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 import * as React from "react";
 
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({
+export function ThemeProvider({
   children,
   ...props
-}) => {
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-};
+}
