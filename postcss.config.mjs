2a9d38fb59a0340da2df6e28fcd1a0bde2786cd9
OrcDev <urosmiric88@gmail.com>
2025-01-26 22:54:23 +0100

    fix(tailwind) update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+export default {
+  plugins: ["@tailwindcss/postcss"],
 };
-
-export default config;
